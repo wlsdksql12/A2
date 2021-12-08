7de12240b03fb3366e08b7a9d929aa42be1b9f11
chl49 <chl49@DESKTOP-UIGJ1HD>
2021-12-08 15:54:18 +0900

    [최성석] jsp 입력

--- cast/src/main/java/com/gdu/controller/CustomerController.java
+++ cast/src/main/java/com/gdu/controller/CustomerController.java
@@ -1,5 +1,11 @@
 package com.gdu.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class CustomerController {
-
+@GetMapping("/index")
+public String index() {
+	return "index.jsp";
+}
 }
--- cast/src/main/java/com/gdu/vo/Customer.java
+++ cast/src/main/java/com/gdu/vo/Customer.java
@@ -1,5 +1,5 @@
 package com.gdu.vo;
 
 public class Customer {
-
+	
 }
