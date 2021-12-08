fcfe5e8ff054bb4d6e54a674e3a341fed4894916
chl49 <chl49@DESKTOP-UIGJ1HD>
2021-12-08 16:37:09 +0900

    [최성석] bootstrap 추가

--- cast/src/main/java/com/gdu/controller/CustomerController.java
+++ cast/src/main/java/com/gdu/controller/CustomerController.java
@@ -2,14 +2,17 @@ package com.gdu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @Controller
 public class CustomerController {
 	
-	@GetMapping("/")
+	@GetMapping("/index")
 	public String index() {
 		return "index";
 	}
+	
 	@GetMapping("/about")
 	public String about() {
 		return "about";
