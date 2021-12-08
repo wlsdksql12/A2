d7038dd9f3ae994c54a65604e176b71b4cfd4d34
chl49 <chl49@DESKTOP-UIGJ1HD>
2021-12-08 18:25:38 +0900

    customer 페이지 템플릿 적용 후 이미지파일 수정 중

--- cast/src/main/java/com/gdu/cast/controller/CustomerController.java
+++ cast/src/main/java/com/gdu/cast/controller/CustomerController.java
@@ -2,31 +2,12 @@ package com.gdu.cast.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class CustomerController {
 	
-	@GetMapping("/about")
-	public String about() {
-		return "about";
-	}
-	
-	@GetMapping("/index")
-	public String index() {
-		return "index";
-	}
-	@GetMapping("/shop")
-	public String shop() {
-		return "shop";
-	}
-	@GetMapping("/contact")
-	public String contact() {
-		return "contact";
-	}
-	@GetMapping("/shopsingle")
+	@GetMapping("/customerIndex")
 	public String shopsingle() {
-		return "shop-single";
+		return "customerIndex";
 	}
-
 }
