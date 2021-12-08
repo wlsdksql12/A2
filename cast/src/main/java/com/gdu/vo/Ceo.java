69010c273d39277962655cea40deb83c38b70922
nyaum <77260194+nyaum@users.noreply.github.com>
2021-12-08 15:31:24 +0900

    Update Ceo.java

--- cast/src/main/java/com/gdu/vo/Ceo.java
+++ cast/src/main/java/com/gdu/vo/Ceo.java
@@ -7,7 +7,7 @@ public class Ceo {
 	private String ceo_id;			// 사업자 아이디
 	private String ceo_name;		// 사업자 이름
 	private String ceo_email;		// 사업자 이메일
-	private String ceo_phonenum;	// 사업자 휴대번호
+	private String ceo_phonenum;	        // 사업자 휴대번호
 	private String ceo_jumin;		// 사업자 주민등록번호
 	private String ceo_license;		// 사업자 등록증 여부
 	private String password;		// 사업자 비밀번호
