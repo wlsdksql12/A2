6fdedd8ece03f91fd5d9b1d1aa248f6176de2c65
uphau <uphau@DESKTOP-B1S8RG4>
2021-12-08 15:30:22 +0900

    [문현준] 사업자 테이블 vo 생성

---
+++ cast/src/main/java/com/gdu/vo/Ceo.java
@@ -0,0 +1,16 @@
+package com.gdu.vo;
+
+import lombok.Data;
+
+@Data
+public class Ceo {
+	private String ceo_id;			// 사업자 아이디
+	private String ceo_name;		// 사업자 이름
+	private String ceo_email;		// 사업자 이메일
+	private String ceo_phonenum;	// 사업자 휴대번호
+	private String ceo_jumin;		// 사업자 주민등록번호
+	private String ceo_license;		// 사업자 등록증 여부
+	private String password;		// 사업자 비밀번호
+	private String create_date;		// 사업자 정보 생성일
+	private String update_date;		// 사업자 정보 수정일
+}
