029af1a4949bfb332781ececa1f81e95ba2d322c
admin <admin@DESKTOP-Q93ILR3>
2021-12-09 13:40:41 +0900

    [최인호] 관리자 공지사항 템플릿 수정

---
+++ cast/src/main/java/com/gdu/cast/controller/NoticeController.java
@@ -0,0 +1,12 @@
+package com.gdu.cast.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NoticeController {
+	@GetMapping("/addNotice")
+	public String addNotice() {
+		return "/admin/addNotice";
+	}
+}
