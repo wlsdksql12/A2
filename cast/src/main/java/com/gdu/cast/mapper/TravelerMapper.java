24c059205fed7c6152afdf2b97c2cd4c3fa9da6b
admin <admin@DESKTOP-43VKCF3>
2021-12-13 15:00:21 +0900

    [전성현] Traveler Mapper, Service, Controller에서 RoomSelect Mapper, Service, Controller로 분리

--- cast/src/main/java/com/gdu/cast/mapper/TravelerMapper.java
+++ cast/src/main/java/com/gdu/cast/mapper/TravelerMapper.java
@@ -1,32 +1,11 @@
 package com.gdu.cast.mapper;
 
-import java.util.List;
-import java.util.Map;
-
 import org.apache.ibatis.annotations.Mapper;
 
-import com.gdu.cast.vo.RoomSelect;
 import com.gdu.cast.vo.Traveler;
 
 @Mapper
 public interface TravelerMapper {
-	// 숙소 추천 추가
-		void insertRoomSelect(RoomSelect roomSelect);
-	
-	// 숙소 추천 삭제
-	int deleteRoomSelect(RoomSelect roomSelect);
-		
-	// 숙소 추천 수정
-	int updateRoomSelect(RoomSelect roomSelect);
-	
-	// 숙소 추천 상세보기
-	RoomSelect selectRoomSelectOne(int RoomSelectId);
-	
-	// 숙소 추천 리스트
-	List<RoomSelect> selectRoomSelectList(Map<String, Object> map);
-	
-	// 숙소 추천 전체 글 수
-	int selectRoomSelectTotalCount(String searchTitle);
 	
 	// 여행작가 로그인
 	Traveler selectTraveler(Traveler traveler);
