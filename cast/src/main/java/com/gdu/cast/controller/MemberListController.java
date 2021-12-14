c51eeb2afe7444c5fcf8f8746831cc2bb0c79452
admin <admin@DESKTOP-Q93ILR3>
2021-12-14 17:27:50 +0900

    [최인호] 관리자 템플릿에서 Topbar, Logout창 을 분리하여 따로 jsp파일을 만들어서 각 관리자가 사용하는 jsp에 분리한 Topbar, Logout창 jsp를 include함, travelerList에 여행작가가 리스트로 출력되도록하고 customerList에 고객이 활성화가 1이면 비활성화버튼을 보여줘서 클릭시 해당 고객의 활성화가 0이되고 반대로 활성화가 0이면 클릭시 해당 고객의 활성화가 1이되도록 함

--- cast/src/main/java/com/gdu/cast/controller/MemberListController.java
+++ cast/src/main/java/com/gdu/cast/controller/MemberListController.java
@@ -16,17 +16,23 @@ public class MemberListController {
 	@Autowired
 	MemberListService memberListService;
 	
+	// 각 페이지 화면에 보일 갯수
+	final int ROW_PER_PAGE = 10;
+	
 	// 고객 리스트 출력
 	@GetMapping("/admin/customerList")
 	public String customerList(Model model,
 			@RequestParam(defaultValue = "1") int currentPage,
 			@RequestParam(required = false) String searchTitle) {
+		// 페이지 수, 검색어 디버깅
 		System.out.println(currentPage + " <-----MemberListController");
 		System.out.println(searchTitle + " <-----MemberListController");
-		final int ROW_PER_PAGE = 10;
+		
+		// 고객 리스트 가져오기
 		Map<String, Object> map = memberListService.getCustomerList(currentPage, ROW_PER_PAGE, searchTitle);
 		System.out.print(map.get("customerList") + " <-----MemberListController");
 		
+		// 고객 리스트 페이지에 리턴 값
 		model.addAttribute("customerList", map.get("customerList"));
 		model.addAttribute("startPage", map.get("startPage"));
 		model.addAttribute("lastPage", map.get("lastPage"));
@@ -36,17 +42,34 @@ public class MemberListController {
 		return"admin/customerList";
 	}
 	
+	// 고객 리스트에서 비활성화 버튼 클릭 시 
+	@GetMapping("/admin/customerDisable")
+	public String customerDisable(String customerId) {
+		memberListService.updateActiveZeroDate(customerId);
+		return"redirect:/admin/customerList";
+	}
+	
+	// 고객 리스트에서 활성화 버튼 클릭 시
+	@GetMapping("/admin/customerActive")
+	public String customerActive(String customerId) {
+		memberListService.updateActiveOneDate(customerId);
+		return"redirect:/admin/customerList";
+	}
+	
 	// 사업자 리스트 출력
 	@GetMapping("/admin/ceoList")
 	public String ceoList(Model model,
 			@RequestParam(defaultValue = "1") int currentPage,
 			@RequestParam(required = false) String searchTitle) {
+		// 페이지 수, 검색어 디버깅
 		System.out.println(currentPage + " <-----MemberListController");
 		System.out.println(searchTitle + " <-----MemberListController");
-		final int ROW_PER_PAGE = 10;
+
+		// 사업자 리스트 가져오기
 		Map<String, Object> map = memberListService.getCeoList(currentPage, ROW_PER_PAGE, searchTitle);
 		System.out.print(map.get("ceoList") + " <-----MemberListController");
 		
+		// 사업자 리스트 페이지에 리턴 값
 		model.addAttribute("ceoList", map.get("ceoList"));
 		model.addAttribute("startPage", map.get("startPage"));
 		model.addAttribute("lastPage", map.get("lastPage"));
@@ -54,5 +77,28 @@ public class MemberListController {
 		model.addAttribute("currentPage", currentPage);
 		model.addAttribute("searchTitle", searchTitle);
 		return"admin/ceoList";
+	}
+	
+	// 사업자 리스트 출력
+	@GetMapping("/admin/travelerList")
+	public String travelerList(Model model,
+			@RequestParam(defaultValue = "1") int currentPage,
+			@RequestParam(required = false) String searchTitle) {
+		// 페이지 수, 검색어 디버깅
+		System.out.println(currentPage + " <-----MemberListController");
+		System.out.println(searchTitle + " <-----MemberListController");
+
+		// 여행작가 리스트 가져오기
+		Map<String, Object> map = memberListService.getTravelerList(currentPage, currentPage, searchTitle);
+		System.out.print(map.get("ceoList") + " <-----MemberListController");
+		
+		// 사업자 리스트 페이지에 리턴 값
+		model.addAttribute("travelerList", map.get("travelerList"));
+		model.addAttribute("startPage", map.get("startPage"));
+		model.addAttribute("lastPage", map.get("lastPage"));
+		model.addAttribute("totalPage", map.get("totalPage"));
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("searchTitle", searchTitle);
+		return"admin/travelerList";
 	}
 }
---
+++ cast/src/main/webapp/WEB-INF/jsp/admin/adminModalLogout.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<!-- Modal Logout 로그아웃 클릭 시 나타나는 메시지 창-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabelLogout">로그아웃!</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>정말로 로그아웃하겠습니까?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-primary" data-dismiss="modal">취소</button>
+            <a href="/logOut" class="btn btn-primary">로그아웃</a>
+          </div>
+        </div>
+      </div>
+    </div>
+	</body>
+</html>
