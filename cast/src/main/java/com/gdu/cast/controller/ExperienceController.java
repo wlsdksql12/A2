6e3caddda10b5e65548cd4f8a9c7a3c2bca1d4e9
KONG <KONG@DESKTOP-M0AA5AN>
2021-12-17 11:24:28 +0900

    [공수현 문현준] 체험 상세페이지 mapper 수정, controller 수정 및 불러오기 확인

--- cast/src/main/java/com/gdu/cast/controller/ExperienceController.java
+++ cast/src/main/java/com/gdu/cast/controller/ExperienceController.java
@@ -20,95 +20,79 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class ExperienceController {
-	@Autowired
-	ExperienceService experienceService;
-	
-	@GetMapping("/ceo/insertExp")
-	public String insertExp(Model model, int addressId) {
-		
-		model.addAttribute("addressId" , addressId);
-		
-		return "/ceo/insertExp";
-	}
-	
-	@PostMapping("/ceo/insertExp")
-	public String insertExp(HttpSession session, Experience experience) {
-		String ceoId = (String) session.getAttribute("loginCeoId");
-		experience.setCeoId(ceoId);
-		System.out.println(ceoId + " << ceoId");
-		experienceService.insertExp(experience);
-		
-		log.debug("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"+ceoId);
-		log.debug("=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-"+experience.toString());
-		
-		return "/ceo/ceoIndex";
-	}
-	
-	@GetMapping("/ceo/insertExpAddress")
-	public String insertAddress() {
-		return "/ceo/insertExpAddress";
-	}
-	
-	@PostMapping("/ceo/insertExpAddress")
-	public String insertAddress(HttpSession session, Address address, Experience experience) {
-		String ceoId = (String) session.getAttribute("loginCeoId");
-		experience.setCeoId(ceoId);
-		
-		experienceService.insertExpAddress(address);
-		
-		return "redirect:/ceo/insertExp?addressId="+address.getAddressId() + "&ceoId=" + experience.getCeoId();
-	}
-	
-	// 체험 리스트
-	@GetMapping("/ceo/experienceList")
-	public String experienceList(Model model, HttpSession session,
-			@RequestParam(defaultValue = "1") int currentPage,
-			@RequestParam(required = false) String searchTitle) {
-			// required = true -> 값이 안넘어오면 에러, required = false -> 안넘어오면 null
-		System.out.println(searchTitle);
-		final int ROW_PER_PAGE = 10;
-		Map<String, Object> map = experienceService.getExperienceList(currentPage, ROW_PER_PAGE, searchTitle);
-		System.out.println(session);
-		System.out.println(session.getAttribute("loginCeoId"));
-		model.addAttribute("experienceList", map.get("experienceList"));
-		model.addAttribute("startPage", map.get("startPage"));
-		model.addAttribute("lastPage", map.get("lastPage"));
-		model.addAttribute("totalPage", map.get("totalPage"));
-		model.addAttribute("currentPage", currentPage);
-		model.addAttribute("searchTitle", searchTitle);
-		model.addAttribute("loginCeoId", session.getAttribute("loginCeoId"));
-		System.out.println(session.getAttribute("loginCeoId") + " 체험리스트 세션값");
-		return "/ceo/experienceList";
-	}
-	
-	// 체험 상세보기
-	@GetMapping("/ceo/experienceOne")
-	public String experienceOne(Model model, Experience experience, Address address) {
-		experienceService.selectExperienceOne(experience.getExperienceId());
-		
-		model.addAttribute("experience", experience);
-		model.addAttribute("address", address);
-		
-		System.out.println(experience + " << Experience");
-		System.out.println(address + "<< Address");
-		
-		return "/ceo/experienceOne";
-	}
-	
+   @Autowired
+   ExperienceService experienceService;
+   
+   @GetMapping("/ceo/insertExp")
+   public String insertExp(Model model, int addressId) {
+      
+      model.addAttribute("addressId" , addressId);
+      
+      return "/ceo/insertExp";
+   }
+   
+   @PostMapping("/ceo/insertExp")
+   public String insertExp(HttpSession session, Experience experience) {
+      String ceoId = (String) session.getAttribute("loginCeoId");
+      experience.setCeoId(ceoId);
+      System.out.println(ceoId + " << ceoId");
+      experienceService.insertExp(experience);
+      
+      log.debug("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"+ceoId);
+      log.debug("=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-"+experience.toString());
+      
+      return "/ceo/ceoIndex";
+   }
+   
+   @GetMapping("/ceo/insertExpAddress")
+   public String insertAddress() {
+      return "/ceo/insertExpAddress";
+   }
+   
+   @PostMapping("/ceo/insertExpAddress")
+   public String insertAddress(HttpSession session, Address address, Experience experience) {
+      String ceoId = (String) session.getAttribute("loginCeoId");
+      experience.setCeoId(ceoId);
+      
+      experienceService.insertExpAddress(address);
+      
+      return "redirect:/ceo/insertExp?addressId="+address.getAddressId() + "&ceoId=" + experience.getCeoId();
+   }
+   
+   // 체험 리스트
+   @GetMapping("/ceo/experienceList")
+   public String experienceList(Model model, HttpSession session,
+         @RequestParam(defaultValue = "1") int currentPage,
+         @RequestParam(required = false) String searchTitle) {
+         // required = true -> 값이 안넘어오면 에러, required = false -> 안넘어오면 null
+      System.out.println(searchTitle);
+      final int ROW_PER_PAGE = 10;
+      Map<String, Object> map = experienceService.getExperienceList(currentPage, ROW_PER_PAGE, searchTitle);
+      System.out.println(session);
+      System.out.println(session.getAttribute("loginCeoId"));
+      model.addAttribute("experienceList", map.get("experienceList"));
+      model.addAttribute("startPage", map.get("startPage"));
+      model.addAttribute("lastPage", map.get("lastPage"));
+      model.addAttribute("totalPage", map.get("totalPage"));
+      model.addAttribute("currentPage", currentPage);
+      model.addAttribute("searchTitle", searchTitle);
+      model.addAttribute("loginCeoId", session.getAttribute("loginCeoId"));
+      System.out.println(session.getAttribute("loginCeoId") + " 체험리스트 세션값");
+      return "/ceo/experienceList";
+   }
+   
+   // 체험 상세보기
+   @GetMapping("/ceo/experienceOne")
+   public String experienceOne(Model model, Address address, int experienceId) {
+      Experience experience = experienceService.selectExperienceOne(experienceId);
+      
+      model.addAttribute("experience", experience);
+      model.addAttribute("address", address);
+      
+      System.out.println(experience + " << Experience");
+      System.out.println(address + " << Address");
+      
+      return "/ceo/experienceOne";
+   }
+   
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
