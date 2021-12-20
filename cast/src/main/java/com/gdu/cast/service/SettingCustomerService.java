bdceebb99318cb65b0f5a0bfc11c0be5f6df6f8a
최성석 <@DESKTOP-8T80BM4>
2021-12-20 15:05:46 +0900

    [최성석] 회원탈퇴시 qna도 같이 삭제하지 않도록 수정.-> 즉 회원테이블에 데이터가 없어도 qna게시글은 남음.

--- cast/src/main/java/com/gdu/cast/service/SettingCustomerService.java
+++ cast/src/main/java/com/gdu/cast/service/SettingCustomerService.java
@@ -2,10 +2,12 @@ package com.gdu.cast.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gdu.cast.mapper.SettingCustomerMapper;
 import com.gdu.cast.vo.Customer;
 
+@Transactional
 @Service
 public class SettingCustomerService {
 	@Autowired SettingCustomerMapper settingCustomerMapper;
